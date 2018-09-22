e4cc209e6da5018426eadfa986ccee0572f9e603
Harvey Delaney <harvey@harveydelaney.com>
2018-09-22 16:43:42 +1000

    feat: added about section - added profile picture - added about me text - added global sass variables - changed background to fixed - removed x scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import './App.css';
 
 import Landing from './sections/landing/landing';
+import About from './sections/about/about';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="app-container">
-        <Landing/>
+        <Landing />
+        <About />
       </div>
     );
   }
---
+++ src/sections/about/about.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+import "./about.css";
+import Portait from "src/assets/images/portrait.jpg";
+
+const renderAboutText = () => (
+  <div className="about-text">
+    <p>Hey there! My name is Harvey Delaney.</p>
+    <p>
+      I'm a software engineer currently working at the RateSetter. I am a
+      fullstack software engineer that prides myself on writing clean, readable,
+      testable, extensible and scalable code.
+    </p>
+    <p>
+      Outside of work, I am always looking to learn new frameworks and languages
+      by watching Pluralsight videos and putting this knowledge into practice by
+      undertaking my personal projects. I have and currently read a number of
+      well-known programming books including Clean Code and The Pragmatic
+      Programmer to learn best practice programming.
+    </p>
+  </div>
+);
+
+const About: React.StatelessComponent = () => {
+  return (
+    <div className="section about">
+      <h2>About Harvey</h2>
+      <div className="content-container">
+        <img className="portrait" src={Portait} />
+        {renderAboutText()}
+      </div>
+    </div>
+  );
+};
+
+export default About;
