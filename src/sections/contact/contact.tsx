8571d0957450ed02250122566f23d25a3b4f20a1
Harvey Delaney <harvey@harveydelaney.com>
2018-09-23 12:17:14 +1000

    WIP: contact

---
+++ src/sections/contact/contact.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import "./contact.css";
+
+// const contactList = [
+
+// ];
+
+const Contact: React.StatelessComponent = () => {
+  return (
+    <div className="section theme">
+      <h2>Contact</h2>
+
+      <div className="content-container contact-container">
+       h
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
