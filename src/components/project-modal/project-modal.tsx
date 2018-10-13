7a8bf186e910ae43139a70d17421e434fbf040b7
Harvey Delaney <harvey@harveydelaney.com>
2018-10-13 13:54:25 +1100

    feat: lots of style fixes for mobile

--- src/components/project-modal/project-modal.tsx
+++ src/components/project-modal/project-modal.tsx
@@ -19,24 +19,34 @@ const renderTechnologies = (skillList: SkillEnum[]) =>
 
 const renderButtons = (url: IUrl) => (
   <div className="button-links">
-    { url.github && <button onClick={() => window.open(url.github)} className="link-button button-github">
-      Github
-      <i className="fa fa-github" />
-    </button> }
-    { url.website && <button onClick={() => window.open(url.website)} className="link-button button-website">
-      Website
-      <i className="fa fa-link" />
-    </button> }
+    {url.github && (
+      <button
+        onClick={() => window.open(url.github)}
+        className="link-button button-github"
+      >
+        Github
+        <i className="fa fa-github" />
+      </button>
+    )}
+    {url.website && (
+      <button
+        onClick={() => window.open(url.website)}
+        className="link-button button-website"
+      >
+        Website
+        <i className="fa fa-link" />
+      </button>
+    )}
   </div>
 );
 
 class ProjectModal extends React.Component<IProps> {
   public componentDidMount() {
-    document.body.classList.add('disable-scroll');
+    document.body.classList.add("disable-scroll");
   }
 
   public componentWillUnmount() {
-    document.body.classList.remove('disable-scroll');
+    document.body.classList.remove("disable-scroll");
   }
 
   public render() {
@@ -51,7 +61,7 @@ class ProjectModal extends React.Component<IProps> {
       },
       modalExitEvent
     } = this.props;
-  
+
     return (
       <div className="project-modal-container" onClick={modalExitEvent}>
         <div className="modal-content-container" onClick={contentClickEvent}>
@@ -59,19 +69,31 @@ class ProjectModal extends React.Component<IProps> {
             className="project-image"
             src={require(`../../assets/images/projects/${img}`)}
           />
-  
+
           <div className="modal-body-container">
             <div className="header-container">
-              <h2 className="modal-heading">{name}</h2>
-              <i className="fa fa-times close-icon" onClick={modalExitEvent} />
+              <div className="primary">
+                <h2 className="modal-heading">{name}</h2>
+                <i
+                  className="fa fa-times close-icon"
+                  onClick={modalExitEvent}
+                />
+              </div>
+
+              <div className="secondary">
+                <p className="released-date">{releaseDate}</p>
+              </div>
             </div>
-  
-            <p className="released-date">{releaseDate}</p>
-            <p>{detailed}</p>
-  
-            <div className="technologies-container">{renderTechnologies(tagList)}</div>
-  
-            {renderButtons(url)}
+
+            <div className="content-container">
+              <p>{detailed}</p>
+
+              <div className="technologies-container">
+                {renderTechnologies(tagList)}
+              </div>
+            </div>
+
+            <div className="footer-container">{renderButtons(url)}</div>
           </div>
         </div>
       </div>
