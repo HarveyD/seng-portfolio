39202eaf9733e2f0a542886b266e5170257da324
Harvey <harveydelaney@yahoo.com.au>
2020-04-19 21:50:57 +1000

    fix: make sure asset paths are correct

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
 
-module.exports = withTypescript(withSass())
+// Change BASE_PATH if you are planning to have serve the static content 
+// of this project on a different path. For example, I'm hosting it on
+// www.harveydelaney.com/seng-portfolio, so I set BASE_PATH=seng-portfolio
+
+module.exports = withTypescript(withSass({
+  assetPrefix: process.env.BASE_PATH || '',
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH || '',
+  }
+}));
