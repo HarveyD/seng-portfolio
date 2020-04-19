d23251be3f46278fd74fefd2d13e8e2bdfabe534
Harvey <harveydelaney@yahoo.com.au>
2020-04-19 21:50:57 +1000

    fix: make sure assepaths are correct

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
 
-module.exports = withTypescript(withSass())
+module.exports = withTypescript(withSass({
+  assetPrefix: process.env.BASE_PATH || '',
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH || '',
+  }
+}));
